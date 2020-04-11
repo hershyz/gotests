1502f76de9db2ccd83906cb42e68e7613f0db197
Hrishikesh Bagalkote <bagalkote.family@outlook.com>
2020-04-11 12:04:18 -0400

    Create vars.go

---
+++ vars.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	//Variable types: bool, string, int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr, float32, float64
+
+	var num1 float64 = 1.5
+	var num2 float64 = 2.5
+
+	fmt.Println(num1 + num2)
+}
